76004140fb11196883e3fbdc2f0908c1ae0d3461
Rishav Bhattacharjee <brishav42@gmail.com>
2022-05-23 17:15:07 -0400

    Added some basic parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub calc);
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", calc::TermParser::new().parse("0x32").unwrap());
+}
+
+#[test]
+fn correct_casting() {
+    assert_eq!(calc::TermParser::new().parse("-23").unwrap(), 18446744073709551593u64);
+    assert_eq!(calc::TermParser::new().parse("23").unwrap(), 23u64);
+    assert_eq!(calc::TermParser::new().parse("0x52").unwrap(), 82u64);
 }
