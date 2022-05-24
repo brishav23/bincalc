1a8c405c19d9c79830276a1b4965f9f0a9865505
Rishav Bhattacharjee <brishav42@gmail.com>
2022-05-23 22:54:17 -0400

    Built syntax tree with calculate function (parenthesis strictly required)

--- src/calc_ast/mod.rs
+++ src/calc_ast/mod.rs
@@ -1,8 +1,22 @@
+#[derive(Debug)]
+pub struct Tree {
+    pub format: Type,
+    pub exp: Box<Term>
+}
+
+#[derive(Debug)]
+pub enum Term {
+    Val(u64),
+    Line(Box<Term>, Operator, Box<Term>)
+}
+
+#[derive(Debug)]
 pub enum Operator {
     Add,
     Subtract,
 }
 
+#[derive(Debug)]
 pub enum Type {
     Decimal,
     Hex,
--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,34 @@
 lalrpop_mod!(pub calc, "/calc/calc.rs");
 
 mod calc_ast;
-use calc_ast::Type;
+use calc_ast::{Term, Operator};
 
 fn main() {
+    let parser = calc::ExprLineParser::new();
     loop {
-        // let (n, t) = calc::ExprParser::new().parse("0x32 > b").expect("syntax error");
-        if let Ok((n, t)) = calc::ExprLineParser::new().parse("0b1101 -> d") {
-            match t {
-                Type::Decimal => {
-                    println!("{}", n);
-                },
-                Type::Binary => {
-                    println!("{:#b}", n);
-                },
-                Type::Hex => {
-                    println!("{:#x}", n);
-                },
-            }
+        if let Ok(tree) = parser.parse("((0b111 + 0x23) + 23) -> d") {
+            // println!("{:?}", tree);
+            let res: u64 = calculate(&*tree.exp);
+            println!("{}", res);
         } else {
             println!("syntax error");
         }
+    }
+}
+
+fn calculate(t: &Term) -> u64 {
+    match t {
+        Term::Line(t1, op, t2) => {
+            let res = match op {
+                Operator::Add => {
+                    calculate(&*t1) + calculate(&*t2)
+                },
+                Operator::Subtract => {
+                    calculate(&*t1) - calculate(&*t2)
+                },
+            };
+            res
+        },
+        Term::Val(n) => *n,
     }
 }
